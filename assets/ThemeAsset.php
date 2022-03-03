1b7aaf09116dd1013a5a7fa7f4d4e81009cd18d8
riskieyasu <darmawa3@gmail.com>
2022-03-04 01:09:20 +0700

    integrasi dashboard

---
+++ assets/ThemeAsset.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 Yiister
+ * @license https://github.com/yiister/yii2-gentelella/blob/master/LICENSE
+ * @link http://gentelella.yiister.ru
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class ThemeAsset extends AssetBundle
+{
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+        'rmrevin\yii\fontawesome\AssetBundle',
+        'app\assets\BootstrapProgressbar',
+        'app\assets\ThemeBuildAsset',
+        'app\assets\ThemeSrcAsset',
+    ];
+}
